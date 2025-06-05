37ad9c7eeadf38889f40ac3b47bf3243f7cb7b13
Кирилл Жуков <kirillsiber@yandex.ru>
2025-06-05 15:37:02 +0700

    Home work 4

---
+++ Task4_9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+
+            /*int b = a;
+            int cn=1;
+
+            while (a>9) { a = a / 10; cn++; }
+
+            while (cn>=0) {b = b % 10; Console.Write(a); }
+
+            */
+            double b = Math.Pow(5,3);
+
+            Console.Write(b);
+
+        }
+    }
+}
